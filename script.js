79ad98af6d3506d09a50c5caa5d64f582ee261af
Sean Pierre Salutem <77522082+salutemsp@users.noreply.github.com>
2021-03-12 21:43:51 +0800

    filed uploaded

---
+++ script.js
@@ -0,0 +1,51 @@
+const stopWatchEl = document.querySelector(".stopwatch");
+const hrsEl = document.querySelector(".hrs");
+const minsEl = document.querySelector(".mins");
+const secsEl = document.querySelector(".secs");
+const msEl = document.querySelector(".ms");
+const buttons = document.querySelector(".buttons");
+const btnStart = document.querySelector(".start");
+const btnReset = document.querySelector(".reset");
+
+let sec = 0;
+let min = 0;
+let hr = 0;
+let ms = 0;
+let stopwatch;
+const displayTime = function () {
+  msEl.textContent = String(ms).padStart(2, "0");
+  secsEl.textContent = String(sec).padStart(2, "0");
+  minsEl.textContent = String(min).padStart(2, "0");
+  hrsEl.textContent = String(hr).padStart(2, "0");
+};
+const startTimer = function () {
+  stopWatch = setInterval(() => {
+    ms += 1;
+    if (ms === 1000) {
+      sec += 1;
+      ms = 0;
+    }
+    if (sec === 60) {
+      min += 1;
+      sec = 0;
+    }
+    if (min === 60) {
+      hr += 1;
+      min = 0;
+    }
+
+    displayTime();
+  }, 1);
+};
+let isTimerRunning = false;
+btnStart.addEventListener("click", function (e) {
+  if (!isTimerRunning) {
+    startTimer();
+    btnStart.textContent = "Pause";
+    isTimerRunning = true;
+  } else {
+    clearInterval(stopWatch);
+    btnStart.textContent = "Start";
+    isTimerRunning = false;
+  }
+});
