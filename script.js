0c6d7ea507439e6e03eeb5f61dc9784985a68d21
Sean Pierre Salutem <77522082+salutemsp@users.noreply.github.com>
2021-03-13 21:18:32 +0800

    removing milisecond and adding reset functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const stopWatchEl = document.querySelector(".stopwatch");
 const hrsEl = document.querySelector(".hrs");
 const minsEl = document.querySelector(".mins");
 const secsEl = document.querySelector(".secs");
-const msEl = document.querySelector(".ms");
+
 const buttons = document.querySelector(".buttons");
 const btnStart = document.querySelector(".start");
 const btnReset = document.querySelector(".reset");
@@ -10,21 +10,16 @@ const btnReset = document.querySelector(".reset");
 let sec = 0;
 let min = 0;
 let hr = 0;
-let ms = 0;
+
 let stopwatch;
 const displayTime = function () {
-  msEl.textContent = String(ms).padStart(2, "0");
   secsEl.textContent = String(sec).padStart(2, "0");
   minsEl.textContent = String(min).padStart(2, "0");
   hrsEl.textContent = String(hr).padStart(2, "0");
 };
 const startTimer = function () {
   stopWatch = setInterval(() => {
-    ms += 1;
-    if (ms === 1000) {
-      sec += 1;
-      ms = 0;
-    }
+    sec += 1;
     if (sec === 60) {
       min += 1;
       sec = 0;
@@ -35,7 +30,7 @@ const startTimer = function () {
     }
 
     displayTime();
-  }, 1);
+  }, 1000);
 };
 let isTimerRunning = false;
 btnStart.addEventListener("click", function (e) {
@@ -48,4 +43,15 @@ btnStart.addEventListener("click", function (e) {
     btnStart.textContent = "Start";
     isTimerRunning = false;
   }
+});
+
+btnReset.addEventListener("click", function () {
+  btnStart.textContent = "Start";
+
+  clearInterval(stopWatch);
+  sec = 0;
+  min = 0;
+  hr = 0;
+  isTimerRunning = false;
+  displayTime();
 });
